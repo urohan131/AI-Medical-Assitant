8fe5a3e500f5e7431f06c82e3545e3fadb9df214
Rohan Kumar <urohan131@gmail.com>
2025-11-01 10:46:34 +0530

    added the requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="U Rohan Kumar",
+    author_email="urohan131@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
